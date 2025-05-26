5098c608f4037f582d891d81e1ff42b86ac81916
Ajin Varughese <ajinvarughese91@gmail.com>
2025-05-26 07:08:54 +0530

    answer updated

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,25 @@
+// vite.config.js
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+      react(),
+      tailwindcss(),
+  ],
+  resolve: {
+    dedupe: ['react', 'react-dom'],
+  },
+  css: {
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true,
+        modifyVars: {
+          'primary-color': '#2469F4',
+          'link-color': '#5C6899',
+        },
+      },
+    },
+  },
 });
